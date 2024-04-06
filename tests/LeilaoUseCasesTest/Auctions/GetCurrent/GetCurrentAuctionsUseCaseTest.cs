876ba1ca1ea1dc7c6c7c89ff3202678d92a61f43
luizambrozini <luizambrozini@gmail.com>
2024-04-05 21:00:25 -0300

    Leilão

---
+++ tests/LeilaoUseCasesTest/Auctions/GetCurrent/GetCurrentAuctionsUseCaseTest.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using Leilao.Applicarion.Entities;
+using Leilao.Applicarion.Repositories.Contracts;
+using Leilao.Applicarion.UseCases.Auctions.GetCurrent;
+using Moq;
+using Xunit;
+
+namespace LeilaoUseCases.Test.Auctions.GetCurrent
+{
+    public class GetCurrentAuctionsUseCaseTest
+    {
+        [Fact]
+        public void Success()
+        {
+            //AAA
+            //Arrange
+            var mock = new Mock<IAuctionRepository>();
+            mock.Setup(i => i.GetCurrent()).Returns(new Auction());
+            var useCase = new GetCurrentAuctionsUseCase(mock.Object);
+            //Act
+            var auction = useCase.Execute();
+            //Assert
+            auction.Should().NotBeNull();
+
+        }
+    }
+}
